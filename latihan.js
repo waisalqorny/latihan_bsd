fbe3f65185854b8e9b3bf7964dbb9d0970f0a249
waisalqorny <waismudzakir@gmail.com>
2025-09-10 15:00:19 +0700

    latihan javascript

---
+++ latihan.js
@@ -0,0 +1,9 @@
+alert("selamat datang di wibesitekuu guyss");
+
+let konfirmasi = confirm("mau lihat projectku??");
+if (konfirmasi) {
+  let umur = prompt("Berapa umurmu?");
+  alert("Wah, umur kamu " + umur + " tahun!");
+} else {
+  alert("Tidak apa-apa, mungkin lain kali 😄");
+}
